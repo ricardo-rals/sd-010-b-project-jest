2292e1c67478f816d219ab53c028634af6e8c378
brunobastosfer <brunobastos@edu.unifor.br>
2021-04-23 15:16:48 -0300

    initial commit and resolved first test

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async() => {
+    // assert.fail();
+    await expect(answerPhone(true)).resolves.toBe('Oi!')
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async() => {
+    // assert.fail();
+    await expect(answerPhone(false)).rejects.toThrowError('Infelizmente não podemos atender...')
   });
 });
