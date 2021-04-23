0292c54dddda389109234ba82731d6c50868a2e0
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-04-23 14:30:12 -0300

    Correçao na implementaçao dos testes do requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -18,5 +18,5 @@ describe('o retorno do telefonema', () => {
   test('ocupado', () =>
     // assert.fail();
     // Insira seu teste assíncrono aqui
-    expect(answerPhone(false)).rejects.toStrictEqual(Error('Infelizmente não podemos atender...')));
+    expect(answerPhone(false)).rejects.toEqual(('Infelizmente não podemos atender...')));
 });
