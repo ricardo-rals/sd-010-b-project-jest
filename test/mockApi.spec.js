54159a6fb4cfdcaaf9a5b089d460b7bd8899a379
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-04-23 16:48:23 -0300

    Requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -34,7 +34,7 @@ describe('verifica o usuário', () => {
     login: {
       username: 'tunicao123',
       password: '1234567890',
-    };
+    },
   }));
 
   test('verifica se o usuário é o tunico', async () => (
