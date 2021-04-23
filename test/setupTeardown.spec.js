23d35142bfead084d834b9de1d80f9443206b945
renatogsouza <renatomacedo1995@gmail.com>
2021-04-23 19:13:44 -0300

    vlw

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,4 +1,4 @@
-// const adventure = require('../src/setupTeardown');
+const adventure = require('../src/setupTeardown');
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
 O grupo parte em direção ao sucesso, mas,
@@ -21,25 +21,22 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
   test('depois da primeira aventura', () => {
-    expect(1).toBe(1);
-    // expect(adventure.specialists.length).toBe(5);
+    expect(adventure.specialists.length).toBe(5);
   });
   test('depois da segunda aventura', () => {
-    expect(1).toBe(1);
-    // expect(adventure.specialists.length).toBe(4);
+    expect(adventure.specialists.length).toBe(4);
   });
   test('depois da terceira aventura', () => {
-    expect(1).toBe(1);
-    // expect(adventure.specialists.length).toBe(3);
+    expect(adventure.specialists.length).toBe(3);
   });
   test('depois da quarta aventura', () => {
-    expect(1).toBe(1);
-    // expect(adventure.specialists.length).toBe(2);
+    expect(adventure.specialists.length).toBe(2);
   });
   test('depois da quinta aventura', () => {
-    expect(1).toBe(1);
-    // expect(adventure.specialists.length).toBe(1);
+    expect(adventure.specialists.length).toBe(1);
   });
 });
