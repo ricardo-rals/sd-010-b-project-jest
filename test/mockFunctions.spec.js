417da774e296f24258fa7c2c034f451ea3e71e61
Cristian <cristian.bugs@gmail.com>
2021-04-23 19:35:41 -0300

    second task saved file

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -28,7 +28,7 @@ describe('verifica as funções e os mocks', () => {
     }
     return fact;
   });
-// reference source: https://www.programiz.com/javascript/examples/factorial
+  // reference source: https://www.programiz.com/javascript/examples/factorial
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
