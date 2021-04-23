94739eb891b4557ec5a6bb1f35a735f11076a149
Carlos <cemargato@gmail.com>
2021-04-23 19:30:14 -0300

    requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  // Recebi a orientação do colega Marconi Moreira T10 e de Luanderson por me explicar o conceito de beforeEach
+  beforeEach(() => {
+  adventure.randomAttack()
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
