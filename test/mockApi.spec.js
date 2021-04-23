8a107b00302b716c5fa08f824ad7e2ebe0c8ef84
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-04-23 16:36:38 -0300

    retornando os valores mockados aos testes

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -22,29 +22,28 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-const apiValues = jest.mock('../src/mockApi', () => jest.fn().mockResolvedValue({
-  results: [
-    {
-      gender: 'male',
-      name: {
-        first: 'Antônio',
-        last: 'Britto',
-      },
-      location: {
-        country: 'Brazil',
-      },
-      email: 'tunico@bol.com.br',
-      login: {
-        username: 'tunicao123',
-        password: '1234567890',
-      },
-    },
-  ],
-}));
+jest.mock('../src/mockApi');
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
-  api.fetchURL = apiValues;
+  const apiValues = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+
+  api.fetchURL = jest.fn().mockResolvedValue(apiValues);
+
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
