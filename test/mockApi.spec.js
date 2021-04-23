d82fc9713e503fb9a762fb7bddaca1bc08c075a2
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-23 19:09:54 -0300

    fetchAPI mockada para testar os valores retornados

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -25,6 +25,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
 
+  // Feito em colaboração com Felippe Corrêa, Renan Braga, Rafael Mathias e João Herculano
+
+  const objReturn = {
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  };
+
+  api.fetchURL = jest.fn().mockResolvedValue(objReturn);
+
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
