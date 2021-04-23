9219ed5854f8bc9a6e1755db58e1a8419686e074
André Arnoni <andre_arnoni@yahoo.com.br>
2021-04-23 18:12:58 -0300

    Executados todos requisitos

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui.
+  test('atende',async () => {
+    const affirmative = await answerPhone(true);
+    expect(affirmative).toEqual('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado',async () => {
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error).toEqual(new Error('Infelizmente não podemos atender...'))
+    }
   });
 });
