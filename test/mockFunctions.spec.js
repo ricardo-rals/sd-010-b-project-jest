79298440147e843bbacdf99a2ba4db2c31e21408
Barrisenn Gonçalo <78623544+Barrisenn@users.noreply.github.com>
2021-04-23 16:14:39 -0300

    Update mockFunctions.spec.js
    Correções mockFunc2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -16,59 +16,68 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add = jest.fn().mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest.fn().mockImplementation((a, b) => a - b);
+  mockFunctions.multiply = jest.fn().mockImplementation((a, b) => a * b);
+  mockFunctions.divide = jest.fn().mockImplementation((a, b) => a / b);
+  mockFunctions.power = jest.fn().mockImplementation((a, b) => {
+    let result = a;
+    if (a !== 0 && b !== 0) {
+      for (let index = 1; index < b; index += 1) {
+        result *= a;
+      }
+      return result;
+    }
+    return 1;
+  });
+  mockFunctions.factorial = jest.fn().mockImplementation((n) => {
+    let result = 1;
+    for (let index = 1; index <= n; index += 1) {
+      result *= index;
+    }
+    return result;
+  });
 
   test('testa função add', () => {
-    const muquiandoAdd = jest.spyOn(mockFunctions, 'add').mockImplementation((a, b) => a + b);
-    expect(muquiandoAdd(1, 2)).toEqual(3);
-    expect(muquiandoAdd(8, 37)).toEqual(45);
-    expect(muquiandoAdd(-11, 25)).toEqual(14);
-    expect(muquiandoAdd(13, -188)).toEqual(-175);
-    expect(muquiandoAdd(7, 26)).toEqual(33);
+    expect(mockFunctions.add(1, 2)).toEqual(3);
+    expect(mockFunctions.add(8, 37)).toEqual(45);
+    expect(mockFunctions.add(-11, 25)).toEqual(14);
+    expect(mockFunctions.add(13, -188)).toEqual(-175);
+    expect(mockFunctions.add(7, 26)).toEqual(33);
   });
   test('testa função subtract', () => {
-    const muquiandoSub = jest.spyOn(mockFunctions, 'subtract').mockImplementation((a, b) => a - b);
-    expect(muquiandoSub(899, 35)).toEqual(864);
-    expect(muquiandoSub(-17, 333)).toEqual(-350);
-    expect(muquiandoSub(45, 97)).toEqual(-52);
-    expect(muquiandoSub(23, -108)).toEqual(131);
-    expect(muquiandoSub(-133, -29)).toEqual(-104);
+    expect(mockFunctions.subtract(899, 35)).toEqual(864);
+    expect(mockFunctions.subtract(-17, 333)).toEqual(-350);
+    expect(mockFunctions.subtract(45, 97)).toEqual(-52);
+    expect(mockFunctions.subtract(23, -108)).toEqual(131);
+    expect(mockFunctions.subtract(-133, -29)).toEqual(-104);
   });
   test('testa função multiply', () => {
-    const muquiandoMul = jest.spyOn(mockFunctions, 'multiply').mockImplementation((a, b) => a * b);
-    expect(muquiandoMul(1, 2)).toEqual(2);
-    expect(muquiandoMul(0, 5)).toEqual(0);
-    expect(muquiandoMul(-4, 9)).toEqual(-36);
-    expect(muquiandoMul(-12, -7)).toEqual(84);
-    expect(muquiandoMul(19, 23)).toEqual(437);
+    expect(mockFunctions.multiply(1, 2)).toEqual(2);
+    expect(mockFunctions.multiply(0, 5)).toEqual(0);
+    expect(mockFunctions.multiply(-4, 9)).toEqual(-36);
+    expect(mockFunctions.multiply(-12, -7)).toEqual(84);
+    expect(mockFunctions.multiply(19, 23)).toEqual(437);
   });
   test('testa função divide', () => {
-    const muquiandoDiv = jest.spyOn(mockFunctions, 'divide').mockImplementation((a,b) => a / b);
-    expect(muquiandoDiv(169, 13)).toEqual(13);
-    expect(muquiandoDiv(-1900, 5)).toEqual(-380);
-    expect(muquiandoDiv(42, 7)).toEqual(6);
-    expect(muquiandoDiv(729, 243)).toEqual(3);
-    expect(muquiandoDiv(1331, 11)).toEqual(121);
+    expect(mockFunctions.divide(169, 13)).toEqual(13);
+    expect(mockFunctions.divide(-1900, 5)).toEqual(-380);
+    expect(mockFunctions.divide(42, 7)).toEqual(6);
+    expect(mockFunctions.divide(729, 243)).toEqual(3);
+    expect(mockFunctions.divide(1331, 11)).toEqual(121);
   });
   test('testa função power', () => {
-    const muquiandoPow = jest.spyOn(mockFunctions, 'power').mockImplementation((a,b) => a ** b);
-    expect(muquiandoPow(10, 2)).toEqual(100);
-    expect(muquiandoPow(2, 10)).toEqual(1024);
-    expect(muquiandoPow(5, 5)).toEqual(3125);
-    expect(muquiandoPow(1, 10)).toEqual(1);
-    expect(muquiandoPow(0, 0)).toEqual(1);
+    expect(mockFunctions.power(10, 2)).toEqual(100);
+    expect(mockFunctions.power(2, 10)).toEqual(1024);
+    expect(mockFunctions.power(5, 5)).toEqual(3125);
+    expect(mockFunctions.power(1, 10)).toEqual(1);
+    expect(mockFunctions.power(0, 0)).toEqual(1);
   });
   test('testa função factorial', () => {
-    const muquiandoFac = jest.spyOn(mockFunctions, 'factorial').mockImplementation((a) => {
-      let fact = a;
-      for (let i = 1; i < a; i += 1) {
-        fact *= i;
-      }
-      return fact;
-    });
-    expect(muquiandoFac(5)).toEqual(120);
-    expect(muquiandoFac(10)).toEqual(3628800);
-    expect(muquiandoFac(3)).toEqual(6);
-    expect(muquiandoFac(8)).toEqual(40320);
-    expect(muquiandoFac(2)).toEqual(2);
+    expect(mockFunctions.factorial(5)).toEqual(120);
+    expect(mockFunctions.factorial(10)).toEqual(3628800);
+    expect(mockFunctions.factorial(3)).toEqual(6);
+    expect(mockFunctions.factorial(8)).toEqual(40320);
+    expect(mockFunctions.factorial(2)).toEqual(2);
   });
 });
