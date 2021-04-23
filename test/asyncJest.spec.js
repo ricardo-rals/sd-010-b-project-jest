df015b880755704e6839a4faa30c3a9088535250
Thiago Marchini <thiago@msn.com>
2021-04-23 14:03:08 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,9 +12,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
-  });
+    expect.assertions(1);
+    return answerPhone(true).then((response) => {
+      expect(response).toBe('Oi!');
+    });
+  }
+    
   test('ocupado', () => {
     assert.fail();
     // Insira seu teste assíncrono aqui
