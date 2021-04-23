e72bd39cb327a08c4db1b8b906cad160e775d1d4
Matheus H. Gois <matheus.hengo@gmail.com>
2021-04-23 15:44:03 -0300

    mockApi() tests done

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,8 +24,25 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
-
-  test('verifica se o usuário é o tunico', async () => (
+  const fetchURL = jest
+    .spyOn(api, 'fetchURL')
+    .mockResolvedValue({
+      gender: 'male',
+      name: {
+        first: 'Antônio',
+        last: 'Britto',
+      },
+      location: {
+        country: 'Brazil',
+      },
+      email: 'tunico@bol.com.br',
+      login: {
+        username: 'tunicao123',
+        password: '1234567890',
+      },
+    });
+  test('verifica se o usuário é o tunico', async () => {
+    expect.assertions(8);
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
@@ -34,6 +51,7 @@ describe('verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+      expect(fetchURL).toHaveBeenCalled();
+    });
+  });
 });
