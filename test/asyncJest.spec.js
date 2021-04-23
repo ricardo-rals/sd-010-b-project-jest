a7e7c9e11c65593243eea9df169b9848b2f30603
Fernando <fhomaia@gmail.com>
2021-04-23 14:46:42 -0300

    Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    const phoneAnswer = await answerPhone(true);
+    expect.assertions(1);
+    expect(phoneAnswer).toEqual('Oi!');
   });
-  test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    try {
+      const phoneAnswer = await answerPhone(false);
+    } catch (error) {
+      expect.assertions(1);
+      expect(error).toEqual(new Error('Infelizmente não podemos atender...'));
+    }
   });
 });
