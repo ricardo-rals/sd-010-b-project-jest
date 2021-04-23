206d684229a748abf03a1c54395f0293c051b3ff
Fabio Marturano <fabiocmjor@gmail.com>
2021-04-23 16:16:45 -0300

    Requisito 3

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -14,10 +14,24 @@ O foco aqui é a utilização de mock functions.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+// Source :https://serprogramador.com.br/artigos/topico/javascript/Como-calcular-a-operacao-matematica-de-fatorial-com-JavaScript
+function newFatoral(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * newFatoral(n - 1);
+}
+
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add = jest.fn().mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest.fn().mockImplementation((a, b) => a - b);
+  mockFunctions.multiply = jest.fn().mockImplementation((a, b) => a * b);
+  mockFunctions.divide = jest.fn().mockImplementation((a, b) => a / b);
+  mockFunctions.power = jest.fn().mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial = jest.fn().mockImplementation(newFatoral);
 
-  test('testa função add', () => {
+  it('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
     expect(mockFunctions.add(-11, 25)).toEqual(14);
