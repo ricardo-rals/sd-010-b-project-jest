f69fe5f24caa840ad06917216e0c64e33bece5a9
denis-rossati <denis.rossati@hotmail.com>
2021-04-23 14:55:27 -0300

    tentando o primeiro requisito de novo... parece que era algo com o lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,6 +10,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
+
   test('atende', () => answerPhone(true).then((response) => {
     expect(response).toBe('Oi!');
   }));
