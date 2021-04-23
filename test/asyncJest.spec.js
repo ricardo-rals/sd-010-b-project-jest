0f3b8c8df3145bac9d2c8a92a6c2855b1ecc0acd
Roger Franco <rogerfrancogalt@gmail.com>
2021-04-23 16:17:26 -0400

    resolução exercício 1 parte 3

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,7 +13,7 @@ describe('o retorno do telefonema', () => {
   test('atende', async () => {
     await expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test('ocupado', async () => {  
-  await expect(answerPhone(false)).rejects.toEqual(Error('Infelizmente não podemos atender...'));
+  test('ocupado', async () => {
+    await expect(answerPhone(false)).rejects.toEqual(Error('Infelizmente não podemos atender...'));
   });
 });
