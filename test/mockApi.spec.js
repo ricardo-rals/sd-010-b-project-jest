a80bb95ecd5ceedc0e0b764dfd61f6f57dfa0ba1
Alan Tanaka <alanctnk@gmail.com>
2021-04-24 10:25:02 -0300

    3rd requirement

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,9 +24,29 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const json = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
 
-  test('verifica se o usuário é o tunico', async () => (
-    api.fetchURL().then((user) => {
+  };
+  test('verifica se o usuário é o tunico', () => {
+    const spyFetchURL = jest
+      .spyOn(api, 'fetchURL')
+      .mockImplementation((data) => new Promise((resolve, _) => {
+        resolve(data);
+      }));
+    return spyFetchURL(json).then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
@@ -34,6 +54,7 @@ describe('verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+      expect.assertions(7);
+    });
+  });
 });
