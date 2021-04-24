b95eca9699e286e316e63cfcfb43cdfb8775e3d7
Marconi <marconieng@gmail.com>
2021-04-24 18:18:18 -0300

    Altera requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,6 +15,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
+  // Escreva aqui o seu teste.
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
