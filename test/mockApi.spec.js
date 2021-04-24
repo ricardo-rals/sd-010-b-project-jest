0fb398b2a674e7d058b363de6f49d2e1f07e3960
Flavio Andrade <foa12381@gmail.com>
2021-04-23 21:58:39 -0300

    implementa requesito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,6 +24,20 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const getObject = Promise.resolve({
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  });
+
+  api.fetchURL = jest.fn(() => getObject);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
@@ -37,5 +51,3 @@ describe('verifica o usuário', () => {
     })
   ));
 });
-
-const testes = jest.fn(() => testes);
