dcd1e54d9be4026d59401a6ae727e13c78c93c79
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-04-24 14:08:27 -0300

    subir correcao lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -4,16 +4,15 @@ const answerPhone = require('../src/asyncJest');
 
 /*
 A função answerPhone recebe um parâmetro boleano.
-Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
+Dependendo do parâmetro o retorno da função varia, veja a função no  arquivo 'src/asyncJest.js'
 
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
 
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos casa da pasta 'src'.
 */
 
-// eslint-disable-next-line quotes
-describe("o retorno do telefonema", () => {
+describe('o retorno do telefonema', () => {
   test('atende', async () => {
     expect.assertions(1);
     return answerPhone('atende').then((atende) => { expect(atende).toBe('Oi!'); });
