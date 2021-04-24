834abcef61745678a3035b7bd613cda1517b7caf
brunobastosfer <brunobastos@edu.unifor.br>
2021-04-24 00:19:42 -0300

    fix lint

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,16 +24,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
-  const getApi = jest.spyOn(api, "fetchURL")
+  const getApi = jest.spyOn(api, 'fetchURL');
   getApi.mockResolvedValue({
     gender: 'male',
     name: { first: 'Antônio', last: 'Britto' },
     location: { country: 'Brazil' },
     email: 'tunico@bol.com.br',
-    login: {username: 'tunicao123' , password: '1234567890'}
-  })
-  test('verifica se o usuário é o tunico', async () => ( 
-      api.fetchURL().then((user) => {
+    login: { username: 'tunicao123', password: '1234567890' },
+  });
+  test('verifica se o usuário é o tunico', async () => (
+    api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
