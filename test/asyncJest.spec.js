d59bb3c66da3ec975453e3b8c59827d6b3698d59
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-04-24 19:08:23 -0300

    requisito 1 - novas correçoes

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,15 +13,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('o retorno do telefonema', () => {
   test('atende', () => {
     // assert.fail();
-    const resp = answerPhone(true).then((answer) => {
+    expect.assertions(1);
+    return answerPhone(true).then((answer) => {
       expect(answer).toBe('Oi!');
-      return resp;
     });
   });
   test('ocupado', () => {
     // assert.fail();
     expect.assertions(1);
     return answerPhone(false).catch((erro) => expect(erro)
-      .toEqual(Error('Infelizmente não podemos atender...')));
+      .toStrictEqual(Error('Infelizmente não podemos atender...')));
   });
 });
