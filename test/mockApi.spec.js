1ac8ad38e98983a9f91a1bcbf0075dc90a6c3d9b
Malbathan <malbathanlima@gmail.com>
2021-04-23 21:53:23 -0300

    mockApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,24 +24,25 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const getUser = Promise.resolve({
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  });
+
+  api.fetchURL = jest.fn(() => getUser);
 
   test('verifica se o usuário é o tunico', async () => (
-    // api.fetchURL = jest.fn().mockReturnValue((user) => user = {
-    //   gender : male,
-    //     name : {
-    //       first : Antônio,
-    //       last : Britto
-    //     },
-    //     location : {
-    //       country : Brazil
-    //     },
-    //     email : 'tunico@bol.com.br',
-    //     login : {
-    //       username : 'tunicao123',
-    //       password : 1234567890
-    //     }
-    // });
-
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
@@ -53,3 +54,6 @@ describe('verifica o usuário', () => {
     })
   ));
 });
+
+// junto ao Flavio e gustavo cerqueira, chegamos a essa conclusão
+// ele nos informou a respeito da Promise, e conseguimos chegar ao exito.
