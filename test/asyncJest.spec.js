bdbe64dcd6c6757dd5c04e4b78c06ef0d808ec53
programadorEmerson <programador.emerson@hotmail.com>
2021-04-25 15:30:45 -0300

    Correções

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,10 +11,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    expect(answerPhone(true)).resolves.toEqual('Oi!');
-  });
-  test('ocupado', () => {
-    expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
-  });
+  test('atende', () => answerPhone(true).then((answer) => {
+    expect(answer).toEqual('Oi!');
+  }));
+  test('ocupado', () => answerPhone(false).catch((error) => {
+    expect(error).toEqual(Error('Infelizmente não podemos atender...'));
+  }));
 });
