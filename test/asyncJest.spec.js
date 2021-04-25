e59fd55972376383c582055b32c8dcb71811a335
Victor Lopes <victor.cabrera0209@gmail.com>
2021-04-25 15:47:23 -0300

    Second try to resolve the evaluator job problem

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,13 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    // assert.fail();
     // Insira seu teste assíncrono aqui
     expect.assertions(1);
     return expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
   test('ocupado', () => {
-    // assert.fail();
     // Insira seu teste assíncrono aqui
     expect.assertions(1);
     const errorString = 'Infelizmente não podemos atender...';
