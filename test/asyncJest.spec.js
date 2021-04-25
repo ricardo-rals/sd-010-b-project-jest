4b92c198f5fbcf03c72665126a9da82c022f5a0c
Daniel Ceci <danielceci96@gmail.com>
2021-04-25 17:30:30 -0300

    ainda refatorando o req 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', async () => { // o req pede pra ser assincrona, aí usa o await também.
+    expect.assertions(1);
     const atendeu = await answerPhone(true);
     expect(atendeu.toBe('Oi!'));
   });
   // A parte abaixo da função com o parâmetro False, eu apenas entendi a solução com o PR  do colega Matheus Bodra. Link do PR dele: https://github.com/tryber/sd-010-b-project-jest/blob/6d0a58f8590dc4c1a3cf8caaf04033c833d852d3/test/asyncJest.spec.js
   test('ocupado', async () => {
+    expect.assertions(1);
     const naoAtende = await answerPhone(false).catch((error) => error); // tem que usar o catch aqui.
     expect(naoAtende).toEqual(Error('Infelizmente não podemos atender...')); // atenção ao código no SRC.
   });
