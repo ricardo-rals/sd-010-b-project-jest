6f13228166c11fe252a368166f31e6aa270ebed5
Breno3B <brenorm@gmail.com>
2021-04-25 18:03:01 -0300

    Requisito 1: Refatoraçao com Resolves/Rejects

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,17 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('o retorno do telefonema', () => {
   test('atende', () => {
-    // Source: https://app.betrybe.com/course/fundamentals/testes-automatizados-com-jest/jest-testes-assincronos/7cf613f2-d8ba-40b5-8f9d-2619d813a106/conteudos/24761f0f-7fe2-4fdc-9a53-de43167217ce/testando-codigos-assincronos/9023bf96-c786-40f2-8056-bbee1923b52e?use_case=side_bar
+    // Source: https://app.betrybe.com/course/fundamentals/testes-automatizados-com-jest/jest-testes-assincronos/7cf613f2-d8ba-40b5-8f9d-2619d813a106/conteudos/24761f0f-7fe2-4fdc-9a53-de43167217ce/matcher-resolves-rejects/a2075773-401d-48f1-bb53-4f617ef63c79?use_case=next_button
     expect.assertions(1);
-    return answerPhone(true).then((resolve) => {
-      expect(resolve).toBe('Oi!');
-    });
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test('ocupado', () => {
-    // Source: https://app.betrybe.com/course/fundamentals/testes-automatizados-com-jest/jest-testes-assincronos/7cf613f2-d8ba-40b5-8f9d-2619d813a106/conteudos/24761f0f-7fe2-4fdc-9a53-de43167217ce/testando-codigos-assincronos/9023bf96-c786-40f2-8056-bbee1923b52e?use_case=side_bar
+    // Source: https://app.betrybe.com/course/fundamentals/testes-automatizados-com-jest/jest-testes-assincronos/7cf613f2-d8ba-40b5-8f9d-2619d813a106/conteudos/24761f0f-7fe2-4fdc-9a53-de43167217ce/matcher-resolves-rejects/a2075773-401d-48f1-bb53-4f617ef63c79?use_case=next_button
     expect.assertions(1);
-    return answerPhone(false).catch((reject) => {
-      expect(reject).toEqual(Error('Infelizmente não podemos atender...'));
-    });
+    return expect(answerPhone(false)).rejects.toEqual(Error('Infelizmente não podemos atender...'));
   });
 });
