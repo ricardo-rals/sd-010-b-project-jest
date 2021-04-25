4f7dd36d72a03a1a9e8605d4eb90bb96db35d306
Carlos Vieira <appencigr@gmail.com>
2021-04-25 13:50:11 -0300

    Resolvendo problema na chamada duplicada da função setupTeardown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -23,18 +23,18 @@ describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
   beforeEach(() => adventure.randomAttack()); // Executa a função antes dos testes.
   test('depois da primeira aventura', () => {
-    expect(adventure.specialists.length).toBe(4);
+    expect(adventure.specialists.length).toBe(5);
   });
   test('depois da segunda aventura', () => {
-    expect(adventure.specialists.length).toBe(3);
+    expect(adventure.specialists.length).toBe(4);
   });
   test('depois da terceira aventura', () => {
-    expect(adventure.specialists.length).toBe(2);
+    expect(adventure.specialists.length).toBe(3);
   });
   test('depois da quarta aventura', () => {
-    expect(adventure.specialists.length).toBe(1);
+    expect(adventure.specialists.length).toBe(2);
   });
   test('depois da quinta aventura', () => {
-    expect(adventure.specialists.length).toBe(0);
+    expect(adventure.specialists.length).toBe(1);
   });
 });
