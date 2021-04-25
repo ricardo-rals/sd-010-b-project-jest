2b8ddda08bb21c2296ce505cbc832cb59d64f209
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-25 03:00:42 -0300

    who survivor

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -19,9 +19,12 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+// Dicas de colegas no Discord  //
 
+describe('quem sobreviveu?', () => {
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
@@ -36,5 +39,7 @@ describe('quem sobreviveu?', () => {
   });
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
+    const { nome, classe } = adventure.specialists[0];
+    console.log(`Quem sobreviveu foi: ${nome}, ${classe}`);
   });
 });
