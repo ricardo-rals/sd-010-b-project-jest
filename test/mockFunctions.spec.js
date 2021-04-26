cd7d7a67db0ee7ea128c18f95f393cf8edd1de1c
CamilaCode7 <CamilaCode7>
2021-04-25 23:22:32 -0300

    adicionando novo test

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,5 +1,7 @@
 const mockFunctions = require('../src/mockFunctions');
 
+jest.mock('../src/mockFunctions');
+
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -15,7 +17,21 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a -b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => {
+  const fact = a;
+  let resultado = fact;
+  for (let i = 1; i < fact; i += 1) {
+    resultado *= i;
+  }
+  return resultado;
+  });
+
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
