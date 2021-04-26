fa9d5e7ef86c53a7ba2ea1ceeba0a453183b69a8
aline <aline.melryaa@gmail.com>
2021-04-26 14:08:36 -0400

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,3 @@
-const assert = require('assert');
-const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -10,13 +8,13 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('o retorno do telefonema', () => {
+describe('o retorno do telefonema', async () => {
   test('atende', () => {
-    assert.fail();
+
     // Insira seu teste assíncrono aqui
   });
   test('ocupado', () => {
-    assert.fail();
+
     // Insira seu teste assíncrono aqui
   });
 });
