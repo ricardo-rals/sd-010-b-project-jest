2ac2f2f0a00680e605986d0007b4ee244537cea9
EderPaiva <edergomesdepaiva12@gmail.com>
2021-04-26 13:28:26 -0300

    tentei implementation once mas nao deu certo com ajuda do eduardo santos, consegui usando o before each

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,7 +21,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
+  beforeEach(() => {
+    adventure.specialists.splice(1, 1);
+  });
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
