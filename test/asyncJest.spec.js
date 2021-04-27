474c115865569eb8827ac8857370bc8d8a2566bd
tomsantos07 <xotmsantosx@live.com>
2021-04-27 13:55:57 -0300

    resolvendo requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -11,12 +11,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  // Utilizei o course para realizar esta parte do exercício.
+  // https://app.betrybe.com/course/fundamentals/testes-automatizados-com-jest/jest-testes-assincronos/7cf613f2-d8ba-40b5-8f9d-2619d813a106/conteudos/24761f0f-7fe2-4fdc-9a53-de43167217ce/testando-codigos-assincronos/9023bf96-c786-40f2-8056-bbee1923b52e?use_case=side_bar
+  test('atende', async () => {
+  // fonte utilizada: https://jestjs.io/pt-BR/docs/expect#expectassertionsn%C3%BAmero
+    expect.assertions(1);
   });
   test('ocupado', () => {
-    assert.fail();
+
     // Insira seu teste assíncrono aqui
   });
 });
