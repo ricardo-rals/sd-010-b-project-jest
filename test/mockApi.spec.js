eca523b327194292ff56af9d6efdfc66c33935cf
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-04-28 12:00:39 -0300

    test

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -22,24 +22,21 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-
-
 describe('verifica o usuário', () => {
-
   api.fetchURL = jest
-  .fn()
-  .mockResolvedValue({
-    gender: 'male',
-    name: { first: 'Antônio', last: 'Britto' },
-    location: {
-      country: 'Brazil',
-    },
-    email: 'tunico@bol.com.br',
-    login: {
-      username: 'tunicao123',
-      password: '1234567890',
-    },
-  });
+    .fn()
+    .mockResolvedValue({
+      gender: 'male',
+      name: { first: 'Antônio', last: 'Britto' },
+      location: {
+        country: 'Brazil',
+      },
+      email: 'tunico@bol.com.br',
+      login: {
+        username: 'tunicao123',
+        password: '1234567890',
+      },
+    });
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
