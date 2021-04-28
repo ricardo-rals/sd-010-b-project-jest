5220ab8614d070793e6cf9bccc58cdd224b76f0a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-04-28 20:50:02 -0300

    verifying user

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,6 +23,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
+  api.fetchURL = jest.fn().mockReturnValue(Promise.resolve({
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  }));
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
