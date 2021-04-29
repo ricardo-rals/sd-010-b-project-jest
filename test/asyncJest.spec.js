cf34e6c7298419a1d51227519756c882247070a1
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2021-04-29 17:27:40 -0300

    Requisito asyncJest 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,13 +11,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    expect.assertions(1);
+    const answer = await answerPhone('oi');
+    expect(answer).toBe('Oi!');
   });
   test('ocupado', () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-    // noice
+    expect.assertions(1);
+    return answerPhone().catch((error) =>
+      expect(error.message).toStrictEqual('Infelizmente não podemos atender...'));
   });
 });
