bfd44b7bcb81242ff1ffcff6db45adb4969ed772
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-04-29 19:03:32 -0300

    Em mockApi.spec.js: documenta o uso de mockResolvedValue que foi utilizado com jest.spyOn para mockar a function "fetchURL" de mockApi e testa se os dados retornados são do usuário Tunico.

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,9 +21,27 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+// jest.spyOn(api, 'fetchURL').mockImplementation((number1, number2) => number1 + number2);
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  /*
+    Material consultado sobre mockResolvedValue
+    https://jestjs.io/pt-BR/docs/mock-function-api#mockfnmockresolvedvaluevalue
+  */
+  jest.spyOn(api, 'fetchURL').mockResolvedValue({
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  });
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
