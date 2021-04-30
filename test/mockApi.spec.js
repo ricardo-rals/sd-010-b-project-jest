b4b6f149ddbbdcc84c1618c64d6a2e30200d7516
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-04-30 13:25:14 -0300

    exercicio tunico

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -19,10 +19,26 @@ presente no README.md do projeto.
 
 Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'
 */
 
 describe('verifica o usuário', () => {
+  const tunico = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+  api.fetchURL = jest.fn().mockResolvedValue(tunico);
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
