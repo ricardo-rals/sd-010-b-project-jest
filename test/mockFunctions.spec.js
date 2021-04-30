a572cde463d93ba49ce2e4f9b1f4d45f3299d989
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-04-30 09:21:18 -0300

    Teste da função power do segundo requisito

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -19,6 +19,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
