8c7c6b2639fb1230651421487e1428c31db0dae7
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-05-10 18:45:58 -0300

    commit do projeto inteiro pois estava sem internet quando fiz =)

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,4 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
+jest.mock('../src/mockFunctions');
 
 /*
 Criamos uma série de funções com eficiência duvidosa.
@@ -15,7 +16,24 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  //para esse exercicio estou usar A e B como parametros padroes como foi mostrado na aula
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  // source : https://www.programiz.com/javascript/examples/factorial
+  mockFunctions.factorial.mockImplementation((a) => {
+    let factorial = a;
+    for (let index = 1; index < a; index += 1) {
+      factorial *= index;
+    }
+    return factorial;
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
