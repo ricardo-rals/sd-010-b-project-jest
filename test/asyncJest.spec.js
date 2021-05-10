3a4f315b5c2e7006c2c82747b27c6555e0f98e00
otacisos <otacisods@gmail.com>
2021-05-10 13:49:47 -0300

    fix SRLint-2

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -2,14 +2,10 @@ const answerPhone = require('../src/asyncJest');
 
 describe('o retorno do telefonema', () => {
   test('atende', async () => {
-    expect.assertions(1);
-    const answer = await answerPhone(true);
     await expect(answerPhone(true)).resolves.toBe('Oi!');
   });
 
-  test('ocupado', async() => {
-    const answer = await answerPhone(false);
-    expect.assertions(1);
+  test('ocupado', async () => {
     await expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
