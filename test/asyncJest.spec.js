d108bbbb042ef16a2e7919478130c94db2421339
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-05-10 19:06:14 -0300

    arrumando o lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -14,8 +14,7 @@ describe('o retorno do telefonema', () => {
   test('atende', () =>
     answerPhone('true').then((answer) => {
       expect(answer).toEqual('Oi!');
-    }),
-);
+    }), );
   test('ocupado', () => {
     answerPhone('false').catch((error) => {
       expect(error).toEqual('Infelizmente não podemos atender...');
