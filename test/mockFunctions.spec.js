c2d3cff7477df31a7f4d406d6559e276091032d8
Daniel Alves <araujodaniel321@gmail.com>
2021-05-11 19:01:07 -0300

    exercises 3 and 4 DOUNE

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,7 +15,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  mockFunctions.add = jest.fn().mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest.fn().mockImplementation((a, b) => a - b);
+  mockFunctions.multiply = jest.fn().mockImplementation((a, b) => a * b);
+  mockFunctions.divide = jest.fn().mockImplementation((a, b) => a / b);
+  mockFunctions.power = jest.fn().mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial = jest.fn().mockImplementation(function fac(a) {
+    return (a < 2) ? 1 : fac(a - 1) * a;
+  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
