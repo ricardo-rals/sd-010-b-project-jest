debfc49b94f7051f518e8b15bee88543143c29fc
LucasPeixoto <peixotolucas516@gmail.com>
2021-05-11 18:28:35 -0300

    lint corrections

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,5 @@
-const { AssertionError } = require('assert');
-const assert = require('assert');
+// const { AssertionError } = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
