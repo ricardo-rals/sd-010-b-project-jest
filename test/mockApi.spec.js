99cc886a9926cf5855ee9edac098bf3ecfa2cfe0
LucasPeixoto <peixotolucas516@gmail.com>
2021-05-12 01:41:20 -0300

    projeto concluido

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -28,7 +28,7 @@ describe('verifica o usuário', () => {
     name: { first: 'Antônio', last: 'Britto' },
     location: { country: 'Brazil' },
     email: 'tunico@bol.com.br',
-    login: { username: 'tunicao123', password: 1234567890 } };
+    login: { username: 'tunicao123', password: '1234567890' } };
   api.fetchURL = jest.fn().mockResolvedValue(tunico);
 
   test('verifica se o usuário é o tunico', async () => (
