aa767973d2c9b16d7d0ecbaca1c0bf7e5845d6e8
otacisos <otacisods@gmail.com>
2021-05-12 08:50:03 -0300

    fix eslint-3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,14 +24,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   const user = {
-    gender:'male',
-    name:{first:'Antônio',last:'Britto'},
-    location:{country:'Brazil'},
-    email:'tunico@bol.com.br',
-    login:{username:'tunicao123', password:'1234567890'}
+    gender : 'male',
+    name: {first : 'Antônio', last : 'Britto'},
+    location : {country : 'Brazil'},
+    email: 'tunico@bol.com.br' ,
+    login: {username: 'tunicao123', password : '1234567890'},
   };
 
-  api.fetchURL = jest.fn().mockResolvedValue(user);
+api.fetchURL = jest.fn().mockResolvedValue(user);
   // Crie sua mock da função fetchURL() aqui
 
   test('verifica se o usuário é o tunico', async () => (
