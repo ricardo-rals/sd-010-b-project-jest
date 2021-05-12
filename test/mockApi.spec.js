cc3726639d7b289ad8f2fad5540f9c4696fe28cf
otacisos <otacisods@gmail.com>
2021-05-12 09:05:31 -0300

    fix eslint-9

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -43,7 +43,7 @@ describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
 
   test('verifica se o usuário é o tunico', async () => (
-    return api.fetchURL().then((user) => {
+    api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
       expect(user.name.last).toEqual('Britto');
